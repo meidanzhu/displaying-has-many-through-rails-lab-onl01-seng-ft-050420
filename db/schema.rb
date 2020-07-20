13595b431d664f875c22dce05ec3602ac627c02c
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-07-20 01:04:27 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200720005144) do
-
-  create_table "appointments", force: :cascade do |t|
-    t.datetime "appointment_datetime"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
-  end
+ActiveRecord::Schema.define(version: 20200720005125) do
 
   create_table "doctors", force: :cascade do |t|
     t.string   "name"
