ca12c54d521e6b5981f591ead813de2c646c0318
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-07-20 00:55:17 +0000

    Automatically backed up by Learn

--- db/migrate/20200720005125_create_patients.rb
+++ db/migrate/20200720005125_create_patients.rb
@@ -1,7 +1,8 @@
 class CreatePatients < ActiveRecord::Migration[5.0]
   def change
     create_table :patients do |t|
-      t.text :content
+      t.string :name
+      t.integer :age
 
       t.timestamps
     end
